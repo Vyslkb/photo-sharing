8f1804b5717998b7f99205e311ae03e590854b6b
David Druker <ddruker@users.noreply.github.com>
2015-11-05 12:46:49 -0500

    seeded first test user and post

--- photo_app/db/seeds.rb
+++ photo_app/db/seeds.rb
@@ -1,3 +1,6 @@
+Post.delete_all
+User.delete_all
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +8,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user1 = User.find_by(id: 2)
+user1.posts.create(caption: "testestest", visibility: true)
