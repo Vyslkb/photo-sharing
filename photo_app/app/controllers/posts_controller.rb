b4edab4ab14d9ac3ecdf1a1c260d14098b177e1c
David Druker <ddruker@users.noreply.github.com>
2015-11-06 14:17:06 -0500

    added photo file upload upload validation for create method

--- photo_app/app/controllers/posts_controller.rb
+++ photo_app/app/controllers/posts_controller.rb
@@ -14,13 +14,21 @@ class PostsController < ApplicationController
 
     def create
         caption = post_params[:caption]
-        photo = post_params[:photo]
+        photo = post_params[:photo]   
+        if photo.blank? != false
+            flash[:error] = 'This is a photo sharing app. Come on'
+            binding.pry 
+            redirect_to new_post_path
+        else
         @post = Post.create(:caption => caption, :photo => photo, :user_id => @user.id)
         redirect_to '/'
+        end
     end
+
 
     def new
         @post = Post.new
+
         # binding.pry
     end
 
